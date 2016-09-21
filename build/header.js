31785d5c61834f96cf7e566aaa83d00c3bfb114b
keenwon <semanwmj@yeah.net>
2016-09-21 15:47:06 +0800

    npm script: add file banner

---
+++ build/header.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const os = require('os');
+const fs = require('fs');
+const async = require('async');
+const bufferConcat = require('buffer-concat');
+const glob = require('glob');
+
+glob('dist/**/*.css', {
+  ignore: 'dist/css/libs/**/*'
+}, function (error, files) {
+    if (error) {
+        throw error;
+    }
+
+    var headerBuf = new Buffer(`/*! \n Build on: ${new Date().toLocaleString()} \n Build by: ${os.userInfo().username} \n*/\n`);
+
+    async.map(files, (file, done) => {
+        fs.readFile(file, (error, data) => {
+            if (error) {
+                throw error;
+            }
+
+            var fileBuf = bufferConcat([headerBuf, data]);
+
+            fs.writeFile(file, fileBuf, error => {
+                if (error) {
+                    throw error;
+                }
+
+                done();
+            });
+        });
+    });
+});
