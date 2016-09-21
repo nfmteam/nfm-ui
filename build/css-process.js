a5a4ca6218ef1e43534fcc5d99cc413ff71003c1
keenwon <semanwmj@yeah.net>
2016-09-21 15:42:41 +0800

    npm script: css minify

---
+++ build/css-process.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const fs = require('fs');
+const path = require('path');
+const glob = require('glob');
+const CleanCSS = require('clean-css');
+
+const options = {
+  keepSpecialComments: 0,
+  compatibility: 'ie7',
+  rebase: false
+};
+
+glob('dist/css/**/*.css', {
+  ignore: 'dist/css/libs/**/*'
+}, function (error, files) {
+  if (error) {
+    throw error;
+  }
+
+  var minified,
+    timestamp = Date.now(),
+    absFilePath;
+
+  files.forEach(file => {
+    minified = new CleanCSS(options).minify([file]).styles;
+
+    // 图片添加版本号
+    minified = minified.replace(/url\(([^)]+)\)/g, 'url($1?v=' + timestamp + ')');
+
+    absFilePath = path.join(process.cwd(), file);
+    fs.writeFileSync(absFilePath, minified);
+  });
+});
