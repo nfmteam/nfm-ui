6fb666e87a4710be34c50d45333b5cd4e066e895
keenwon <semanwmj@yeah.net>
2016-09-21 15:48:31 +0800

    格式化js

--- build/header.js
+++ build/header.js
@@ -9,27 +9,27 @@ const glob = require('glob');
 glob('dist/**/*.css', {
   ignore: 'dist/css/libs/**/*'
 }, function (error, files) {
-    if (error) {
+  if (error) {
+    throw error;
+  }
+
+  var headerBuf = new Buffer(`/*! \n Build on: ${new Date().toLocaleString()} \n Build by: ${os.userInfo().username} \n*/\n`);
+
+  async.map(files, (file, done) => {
+    fs.readFile(file, (error, data) => {
+      if (error) {
         throw error;
-    }
+      }
 
-    var headerBuf = new Buffer(`/*! \n Build on: ${new Date().toLocaleString()} \n Build by: ${os.userInfo().username} \n*/\n`);
+      var fileBuf = bufferConcat([headerBuf, data]);
 
-    async.map(files, (file, done) => {
-        fs.readFile(file, (error, data) => {
-            if (error) {
-                throw error;
-            }
+      fs.writeFile(file, fileBuf, error => {
+        if (error) {
+          throw error;
+        }
 
-            var fileBuf = bufferConcat([headerBuf, data]);
-
-            fs.writeFile(file, fileBuf, error => {
-                if (error) {
-                    throw error;
-                }
-
-                done();
-            });
-        });
+        done();
+      });
     });
+  });
 });
