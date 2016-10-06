471db7530d8f9b71d9f4ee53158af12dd77ba023
keenwon <semanwmj@yeah.net>
2016-10-06 15:41:19 +0800

    载入动画添加到index中

--- dev/js/index.js
+++ dev/js/index.js
@@ -13,6 +13,7 @@ var app = {
   cacheElements: function () {
     var self = this;
 
+    self.$laod = $('.nfm-load');
     self.$loadingbar = $('.nfm-loadingbar');
     self.$message = $('.nfm-message');
   },
@@ -20,15 +21,20 @@ var app = {
   bindEvents: function () {
     var self = this;
 
+    // end
+    setTimeout(function () {
+      self.$laod.addClass('end');
+    }, 3000);
+
     // loadingbar
     setTimeout(function () {
       self.$loadingbar.addClass('end');
-    }, 3000);
+    }, 5000);
 
     // show message
     setTimeout(function () {
       self.$message.addClass('show');
-    }, 1000);
+    }, 4000);
 
     // close message
     self.$message.on('click', '.close', function () {
