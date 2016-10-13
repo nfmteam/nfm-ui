2df01ac3e07ed03342f6a8c0fa6d45720163746b
keenwon <semanwmj@yeah.net>
2016-10-13 14:43:44 +0800

    添加初始化页面，模板添加block title

--- bin/router.js
+++ bin/router.js
@@ -16,4 +16,11 @@ router.get('/statistic', function* () {
   this.render('statistic');
 });
 
+/**
+ * Initialize
+ */
+router.get('/initialize', function* () {
+  this.render('initialize');
+});
+
 module.exports = router;
