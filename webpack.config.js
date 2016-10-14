3affeacea92b19daa2750ab967e371be081d85eb
keenwon <semanwmj@yeah.net>
2016-10-14 10:23:18 +0800

    编译js，发布github page

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,38 @@
 'use strict';
 
-var path = require('path');
+const path = require('path');
+const webpack = require('webpack');
 
 function getPath(jsPath) {
   return path.join(__dirname, jsPath);
 }
 
-module.exports = [
-  {
-    entry: {
-      'index': getPath('dev/js/index'),
-      'statistic': getPath('dev/js/statistic'),
-      'system': getPath('dev/js/system')
-    },
-    output: {
-      path: getPath('dist/js/'),
-      filename: '[name].js'
-    }
+var config = {
+  entry: {
+    'index': getPath('dev/js/index'),
+    'statistic': getPath('dev/js/statistic'),
+    'system': getPath('dev/js/system')
+  },
+  output: {
+    path: getPath('docs/script/'),
+    filename: '[name].js'
   }
-];
+};
+
+if (process.env.NODE_ENV === 'docs') {
+  config.devtool = 'eval-cheap-module-source-map';
+} else {
+  let uglifyJsPlugin = new webpack.optimize.UglifyJsPlugin({
+    compress: {
+      warnings: false
+    }
+  });
+
+  config.plugins = config.plugins
+    ? config.plugins.concat([
+      uglifyJsPlugin
+    ])
+    : [uglifyJsPlugin];
+}
+
+module.exports = config;
